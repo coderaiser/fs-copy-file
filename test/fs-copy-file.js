52833971f2a1f64cb1275d0b208a1a9b341c07a2
coderaiser <coderaiser@cloudcmd.io>
2018-05-02 18:50:57 +0300

    test(fs-copy-file) node v10 support

--- test/fs-copy-file.js
+++ test/fs-copy-file.js
@@ -3,88 +3,80 @@
 const path = require('path');
 const fs = require('fs');
 const test = require('tape');
+const tryToCatch = require('try-to-catch');
 const {promisify} = require('util');
-const copyFile = require('../lib/fs-copy-file');
+
+const copyFile = require('..');
 
 const {COPYFILE_EXCL} = fs.constants;
 
 const fixture = path.join(__dirname, 'fixture');
 const noop = () => {};
 
-test('fs.copyFile: no args: message', (t) => {
-    promisify(fs.copyFile)().catch((e) => {
-        promisify(copyFile)().catch((error) => {
-            t.equal(e.message, error.message, 'should throw when no callback');
-            t.end();
-        });
-    });
-});
-
-test('copyFile: no args: message', (t) => {
-    const original = fs.copyFile;
+test('fs.copyFile: no args: message', async (t) => {
+    const copyFileOriginal = fs.copyFile;
     fs.copyFile = null;
-    const copyFile = rerequire('../lib/fs-copy-file');
-    const fn = promisify(copyFile);
     
-    promisify(original)().catch((e) => {
-        fn().catch((error) => {
-            fs.copyFile = original;
-            
-            t.equal(e.message, error.message, 'should throw when no callback');
-            t.end();
-        });
-    });
+    const copyFile = promisify(rerequire('..'));
+    const _copyFile = promisify(copyFile);
+    
+    const msg = 'The "callback" argument must be of type function';
+    const [e] = await tryToCatch(_copyFile);
+    
+    fs.copyFile = copyFileOriginal;
+    
+    t.equal(e.message, msg, 'should throw when no callback');
+    t.end();
 });
 
-test('copyFile: no args: code', (t) => {
-    const original = fs.copyFile;
+test('copyFile: no args: code', async (t) => {
+    const copyFileOriginal = fs.copyFile;
     fs.copyFile = null;
-    const copyFile = rerequire('../lib/fs-copy-file');
-    const fn = promisify(copyFile);
     
-    promisify(original)().catch((e) => {
-        fn().catch((error) => {
-            fs.copyFile = original
-            t.equal(e.code, error.code, 'should code be equal');
-            t.end();
-        });
-    });
+    const copyFile = promisify(rerequire('..'));
+    const _copyFile = promisify(copyFile);
+    
+    const code = 'ERR_INVALID_CALLBACK';
+    const [e] = await tryToCatch(_copyFile);
+    
+    fs.copyFile = copyFileOriginal;
+    
+    t.equal(e.code, code, 'should throw when no callback');
+    t.end();
 });
 
-test('copyFile: no dest', (t) => {
+test('copyFile: no dest', async (t) => {
     const src = '1';
     const dest = 0;
     
     const original = fs.copyFile;
     fs.copyFile = null;
     const copyFile = rerequire('../lib/fs-copy-file');
-    const fn = promisify(copyFile);
+    const _copyFile = promisify(copyFile);
     
-    promisify(original)(src, dest, noop).catch((e) => {
-        fn(src, dest, noop).catch((error) => {
-            fs.copyFile = original;
-            t.equal(e.message, error.message, 'should throw when no dest');
-            t.end();
-        });
-    });
+    const [e] = await tryToCatch(_copyFile, src, dest);
+    const msg = 'The "dest" argument must be one of type string, Buffer, or URL. Received type number';
+    fs.copyFile = original;
+    
+    t.equal(e.message, msg, 'should throw when dest not string');
+    t.end();
 });
 
-test('copyFile: no src', (t) => {
+test('copyFile: no src', async (t) => {
     const src = 1;
     const dest = '0';
     
     const original = fs.copyFile;
     fs.copyFile = null;
     const copyFile = rerequire('../lib/fs-copy-file');
-    const fn = promisify(copyFile);
+    const _copyFile = promisify(copyFile);
     
-    promisify(original)(src, dest, noop).catch((e) => {
-        fn(src, dest, noop).catch((error) => {
-            fs.copyFile = original;
-            t.equal(e.message, error.message, 'should throw when no dest');
-            t.end();
-        });
-    });
+    const [e] = await tryToCatch(_copyFile, src, dest);
+    const msg = 'The "src" argument must be one of type string, Buffer, or URL. Received type number';
+    fs.copyFile = original;
+    
+    t.equal(e.message, msg, 'should throw when src not string');
+    t.end();
 });
 
 test('copyFile: no src', (t) => {
